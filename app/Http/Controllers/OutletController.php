d395ab1958e6bfc4972f736a7705b2692a4ad6c4
Zidanee <ray.zidane9@gmail.com>
2022-02-17 10:36:13 +0700

    first commit

--- app/Http/Controllers/OutletController.php
+++ app/Http/Controllers/OutletController.php
@@ -30,30 +30,30 @@ class OutletController extends Controller
         return view('admin.outlet.edit', compact('outlet', 'outlets'));
     }
 
-    // public function store(Request $request)
-    // {
-    //     Outlet::updateOrCreate(
-    //         ['id' => $request->id],
-    //         [
-    //             'name' => $request->name,
-    //             'address' => $request->address,
-    //             'phone' => $request->phone
-    //         ],
-    //     );
-
-    //     return redirect('outlets');
-    // }
-
     public function store(Request $request)
     {
-        $outlet = new Outlet;
-        $outlet->name = $request->name;
-        $outlet->address = $request->address;
-        $outlet->phone = $request->phone;
-        $outlet->save();
+        Outlet::updateOrCreate(
+            ['id' => $request->id],
+            [
+                'name' => $request->name,
+                'address' => $request->address,
+                'phone' => $request->phone
+            ],
+        );
 
         return redirect('outlets');
     }
+
+    // public function store(Request $request)
+    // {
+    //     $outlet = new Outlet;
+    //     $outlet->name = $request->name;
+    //     $outlet->address = $request->address;
+    //     $outlet->phone = $request->phone;
+    //     $outlet->save();
+
+    //     return redirect('outlets');
+    // }
 
     public function delete($id)
     {
